72ad16b57e61a3da21a191d64cef1c5d1b8472ff
Eduardo <eduardonpicolo@gmail.com>
2021-04-02 19:02:34 -0300

    Move useRequest hook to hooks folder
    - Add initialData option

---
+++ goledger-challenge/hooks/useRequest.js
@@ -0,0 +1,14 @@
+import useSWR from 'swr';
+
+const useRequest = (key, fetcher, initialData) => {
+  const { data, error } = useSWR(key, fetcher, { initialData });
+
+  return {
+    data,
+    isLoading: !error && !data,
+    isRejected: error,
+  };
+};
+
+
+export default useRequest;
