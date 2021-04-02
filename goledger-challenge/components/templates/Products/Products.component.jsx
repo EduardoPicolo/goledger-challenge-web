11ba951b08d11b1618e52172d07f37abd0031b5d
Eduardo <eduardonpicolo@gmail.com>
2021-04-02 19:08:46 -0300

    ProductsComponent: Add navigation to product page
    - Add pre-fetch data

--- goledger-challenge/components/templates/Products/Products.component.jsx
+++ goledger-challenge/components/templates/Products/Products.component.jsx
@@ -1,40 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useRouter } from 'next/router';
 import Button from '../../Button/Button.component';
 import CardList from '../../CardList/CardList.component';
 import { searchAsset } from '../../../services/assetsServices';
 import { ProductsContainer } from './Products.styles';
+import useRequest from '../../../hooks/useRequest';
+import { SEARCH_ASSET } from '../../../services/endpoints';
 
-const Products = () => {
-  const [{ data, isLoading, isRejected }, setStatus] = useState({
-    data: null,
-    isLoading: true,
-    isRejected: false,
-  });
+const body = {
+  query: {
+    selector: {
+      '@assetType': 'product',
+    },
+  },
+};
 
-  useEffect(() => {
-    searchAsset({
-      query: {
-        selector: {
-          '@assetType': 'product',
-        },
-      },
-    }).then((res) => setStatus({ data: res.result, isLoading: false, isRejected: false }));
-  }, []);
+const Products = ({ products }) => {
+  const router = useRouter();
+
+  const { data, isLoading, isRejected } = useRequest(SEARCH_ASSET, searchAsset(body), products);
+
+  const redirect = (id) => router.push('/product/[id]', `/product/${id}`);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   if (isRejected) {
+    // console.error(isRejected);
     return <h1>ERROR!</h1>;
   }
 
   return (
     <ProductsContainer>
       <Button>Add Product</Button>
-      <CardList assets={data} />
+      <CardList assets={data.result} onClick={redirect} />
     </ProductsContainer>
   );
 };
+
+export async function getStaticProps() {
+  const products = await searchAsset({
+    query: {
+      selector: {
+        '@assetType': 'product',
+      },
+    },
+  });
+  return { props: { products } };
+}
 
 export default Products;
