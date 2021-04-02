a4b413febba5792163ac8a771b38bb4cc32a835f
Eduardo <eduardonpicolo@gmail.com>
2021-04-02 19:10:51 -0300

    Add onClick functionality

--- goledger-challenge/components/CardList/CardList.component.jsx
+++ goledger-challenge/components/CardList/CardList.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '../Card/Card.component';
 import CardListContainer from './CardList.styles';
 
-const CardList = ({ assets }) => (
+const CardList = ({ assets, onClick = () => {} }) => (
   <CardListContainer>
     {assets
       ? assets.map((item) => (
@@ -10,6 +10,7 @@ const CardList = ({ assets }) => (
           code={item.code}
           assetName={item.name}
           info={item.price}
+          onClick={() => onClick(item['@key'])}
           key={item['@key']}
         />
       ))
