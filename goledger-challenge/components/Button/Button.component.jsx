d59947741699d3516c6f2408d86e276606c38089
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 19:21:45 -0300

    Add Button prop-types

--- goledger-challenge/components/Button/Button.component.jsx
+++ goledger-challenge/components/Button/Button.component.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ButtonContainer from './Button.styles';
 
 const Button = ({ children, ...props }) => (
   <ButtonContainer {...props}>{children}</ButtonContainer>
 );
+
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
 
 export default Button;
