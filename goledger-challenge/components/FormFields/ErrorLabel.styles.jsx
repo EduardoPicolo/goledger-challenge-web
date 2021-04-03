32764f10e8f4d3e2f3db04944184a9c41dd8c4bb
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 01:59:30 -0300

    Create custom ErrorLabel component

---
+++ goledger-challenge/components/FormFields/ErrorLabel.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ErrorContainer = styled.span`
+  color: red;
+  position: absolute;
+  align-self: center;
+  bottom: -1px;
+  font-weight: 600;
+`;
+
+export default ErrorContainer;
