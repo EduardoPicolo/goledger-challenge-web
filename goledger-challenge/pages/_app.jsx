63ddcbb1512e18ee8415e142139661928b3fd060
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 02:43:09 -0300

    Add ToastContainer in App tree

--- goledger-challenge/pages/_app.jsx
+++ goledger-challenge/pages/_app.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import '../styles/base.scss';
 import PropTypes from 'prop-types';
+import { ToastContainer } from 'react-toastify';
+import toast from '../components/Toast/Toast.component';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/base.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  React.useEffect(() => {
+    toast({ type: 'info', message: 'Welcome!' });
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        closeButton={false}
+        pauseOnFocusLoss
+        pauseOnHover
+        newestOnTop={false}
+        autoClose={3500}
+      />
+    </>
+  );
 }
 
 App.propTypes = {
