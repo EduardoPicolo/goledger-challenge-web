6652cd1505d8ef164d0bdb792f0e14d24db73196
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 20:17:11 -0300

    Create Seller page
    - implement edit seller

--- goledger-challenge/pages/seller/[id].jsx
+++ goledger-challenge/pages/seller/[id].jsx
@@ -10,18 +10,18 @@ import Text from '../../components/Text/Text.styles';
 import Heading from '../../components/Text/Heading.styles';
 import formatDate from '../../utils/formatDate';
 import Spinner from '../../components/Spinner/Spinner.component';
-import EditProductModal from '../../components/templates/EditProductModal/EditProductModal';
+import EditSellertModal from '../../components/templates/EditSellerModal/EditSellerModal.component';
 import DeleteAssetModal from '../../components/templates/DeleteAssetModal/DeleteAssetModal.component';
 
-const Products = ({ id, sellers }) => {
+const Products = ({ id }) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   const {
-    data: product, isLoading, isRejected, mutate,
+    data: seller, isLoading, isRejected, mutate,
   } = useRequest(
     id,
-    readAsset('product', id),
+    readAsset('seller', id),
   );
 
   const router = useRouter();
@@ -42,67 +42,33 @@ const Products = ({ id, sellers }) => {
       <Flex flexDirection="column" width="auto" m="0 0 2.5rem">
         <Row margin="0 0 1rem">
           <Text size="1.2rem" weight="700">
-            Product Details
-          </Text>
-        </Row>
-        <Row>
-          <Text>
-            code:
-            {product?.code}
-          </Text>
-        </Row>
-        <Row>
-          <Text>
-            price: $
-            {product?.price}
-          </Text>
-        </Row>
-      </Flex>
-
-      <Flex flexDirection="column" width="auto" m="0 0 2.5rem">
-        <Row margin="0 0 1rem">
-          <Text size="1.2rem" weight="700">
             Seller Details
           </Text>
         </Row>
         <Row>
           <Text>
             name:
-            {product?.soldBy.name}
+            {seller?.name}
           </Text>
         </Row>
         <Row>
           <Text>
             cnpj:
-            {product?.soldBy.cnpj}
+            {seller?.cnpj}
           </Text>
         </Row>
         <Row>
           <Text>
             address:
-            {product?.soldBy.address}
+            {seller?.address}
           </Text>
         </Row>
         <Row>
           <Text>
             Member since:
-            {product?.soldBy?.dateJoined ? formatDate(product.soldBy.dateJoined) : null}
+            {seller?.dateJoined ? formatDate(seller.dateJoined) : null}
           </Text>
         </Row>
-      </Flex>
-
-      <Flex flexDirection="column" width="auto" m="0 0 2.5rem">
-        <Row margin="0 0 1rem">
-          <Text size="1.2rem" weight="700">
-            Categories
-          </Text>
-        </Row>
-        {(product?.categories
-          && product?.categories.map((category) => (
-            <Row key={category['@key']}>
-              <Text>{category.name}</Text>
-            </Row>
-          ))) || <Text>None</Text>}
       </Flex>
     </>
   );
@@ -119,7 +85,7 @@ const Products = ({ id, sellers }) => {
       rgba(17, 17, 26, 0.05) 0px 8px 32px;"
       >
         <Row margin="0 0 2rem" display="flex" justify="space-between">
-          <Heading horizontalLine>{product?.name || 'Loading...'}</Heading>
+          <Heading horizontalLine>{seller?.name || 'Loading...'}</Heading>
           <Flex width="auto">
             <Button
               m="0 2rem"
@@ -147,16 +113,15 @@ const Products = ({ id, sellers }) => {
           </Button>
         </Row>
       </Flex>
-      <EditProductModal
+      <EditSellertModal
         isOpen={isEditModalOpen}
-        product={product}
-        sellers={sellers}
+        seller={seller}
         mutate={mutate}
         onClose={() => setIsEditModalOpen(false)}
       />
       <DeleteAssetModal
         isOpen={isDeleteModalOpen}
-        asset={product}
+        asset={seller}
         onClose={() => setIsDeleteModalOpen(false)}
       />
     </Container>
