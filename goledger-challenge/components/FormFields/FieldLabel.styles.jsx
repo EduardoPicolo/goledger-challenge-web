3c51c2964d0a65d6d4aaf3a6e3b410e8c0f497b8
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 01:59:49 -0300

    Create custom input Label component

---
+++ goledger-challenge/components/FormFields/FieldLabel.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const LabelContainer = styled.label`
+  text-align: left;
+  font-size: 1rem;
+  text-transform: capitalize;
+  margin-left: 0.15rem;
+  color: #444;
+  margin-bottom: 0.2rem;
+`;
+
+export default LabelContainer;
