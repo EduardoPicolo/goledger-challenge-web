445e710e9f88cfe4e9b88e087815e08b73dc71cd
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 20:14:58 -0300

    Create Sellers component
    - Implement add seller

---
+++ goledger-challenge/components/templates/Sellers/Sellers.component.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+
+import useRequest from '../../../hooks/useRequest';
+import { createAsset, searchAsset } from '../../../services/assetsServices';
+import { SEARCH_ASSET } from '../../../services/endpoints';
+import { SellersContainer } from './Sellers.styles';
+import Button from '../../Button/Button.component';
+import CardList from '../../CardList/CardList.component';
+import Modal from '../../Modal/Modal.component';
+import Input from '../../FormFields/Input/Input.component';
+import toast from '../../Toast/Toast.component';
+import Spinner from '../../Spinner/Spinner.component';
+import Row from '../../Container/Row.styles';
+
+const body = (type) => ({
+  query: {
+    selector: {
+      '@assetType': type,
+    },
+  },
+});
+
+const Sellers = () => {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const {
+    handleSubmit, register, errors, formState, clearErrors,
+  } = useForm();
+
+  const {
+    data: sellers, isLoading, isRejected, mutate,
+  } = useRequest(
+    `${SEARCH_ASSET}sellers`,
+    searchAsset(body('seller')),
+  );
+
+  const onSubmit = handleSubmit(async (formData) => {
+    const payload = {
+      ...formData,
+      '@assetType': 'seller',
+      dateJoined: new Date().toISOString(),
+    };
+    try {
+      const res = await createAsset(payload);
+      await mutate({ ...sellers, result: [...sellers.result, ...res] });
+      toast({
+        type: 'success',
+        message: `${res[0]?.name} was successfully added!`,
+      });
+    } catch (error) {
+      // console.error(error);
+      toast({ type: 'error', message: `An erro has occurred. ${error}` });
+    }
+  });
+
+  const redirect = (id) => router.push('/seller/[id]', `/seller/${id}`);
+
+  if (isRejected) {
+    // console.error(isRejected);
+    return <h1>ERROR!</h1>;
+  }
+
+  return (
+    <SellersContainer>
+      <Row>
+        <Button onClick={() => setIsModalOpen(true)}>Add Seller</Button>
+      </Row>
+      {isLoading ? (
+        <Row justify="center">
+          <Spinner size="100px" width="2.5" />
+        </Row>
+      ) : (
+        <CardList assets={sellers.result} onClick={redirect} />
+      )}
+      <Modal
+        hidden={!isModalOpen}
+        title="ADD SELLER"
+        confirmMessage="CONFIRM"
+        confirmType="submit"
+        disabled={formState.isSubmitting}
+        closeMessage="Cancel"
+        onClose={() => {
+          clearErrors();
+          setIsModalOpen(false);
+        }}
+        onConfirm={() => {
+          onSubmit();
+        }}
+      >
+        <form>
+          <Input
+            type="text"
+            label="Seller name"
+            name="name"
+            placeholder="XYZ"
+            register={register({
+              required: 'Campo obrigatório',
+            })}
+            errors={errors}
+            autoComplete="off"
+          />
+
+          <Input
+            type="number"
+            label="cnpj"
+            name="cnpj"
+            placeholder=""
+            register={register({
+              required: 'Campo obrigatório',
+            })}
+            errors={errors}
+            autoComplete="off"
+          />
+
+          <Input
+            type="text"
+            label="address"
+            name="address"
+            placeholder=""
+            register={register({
+              required: 'Campo obrigatório',
+            })}
+            errors={errors}
+            autoComplete="off"
+          />
+        </form>
+        {formState.isSubmitting ? <Spinner /> : null}
+      </Modal>
+    </SellersContainer>
+  );
+};
+
+export default Sellers;
---
+++ goledger-challenge/components/templates/Sellers/Sellers.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SellersContainer = styled.div`
+  button {
+    margin-bottom: 2rem;
+  }
+`;
