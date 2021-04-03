f3c2af2a16ed5105aacdf10b9ec7e3d1c9f8cc5d
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 20:15:35 -0300

    Add Sellers component in Directory

--- goledger-challenge/components/Directory/Directory.component.jsx
+++ goledger-challenge/components/Directory/Directory.component.jsx
@@ -3,16 +3,16 @@ import { useRouter } from 'next/router';
 
 import DirectoryContainer from './Directory.styles';
 import Products from '../templates/Products/Products.component';
+import Sellers from '../templates/Sellers/Sellers.component';
 
 const Directory = () => {
   const router = useRouter();
-  // console.log(router.pathname, router.query.assetType);
 
   return (
     <DirectoryContainer>
       {router.query.assetType === 'products' && <Products />}
       {router.query.assetType === 'categories' && <div>category</div>}
-      {router.query.assetType === 'sellers' && <div>sellers</div>}
+      {router.query.assetType === 'sellers' && <Sellers />}
     </DirectoryContainer>
   );
 };
