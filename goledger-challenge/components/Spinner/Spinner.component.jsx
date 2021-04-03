1f7194302da3dbf9a3c6b1bf4f59a0f86bba1243
Eduardo <eduardonpicolo@gmail.com>
2021-04-03 03:28:23 -0300

    Create Spinner component

---
+++ goledger-challenge/components/Spinner/Spinner.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import StyledSpinner from './Spinner.styles';
+
+const Spinner = ({ size }) => (
+  <StyledSpinner viewBox="0 0 50 50" size={size}>
+    <circle
+      className="path"
+      cx="25"
+      cy="25"
+      r="20"
+      fill="none"
+      strokeWidth="2"
+    />
+  </StyledSpinner>
+);
+
+export default Spinner;
