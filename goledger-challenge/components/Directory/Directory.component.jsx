78f4de827b3794241ec6bcd913d4ad1e3e0b11d4
Eduardo <eduardonpicolo@gmail.com>
2021-04-04 04:56:48 -0300

    Add Category page in the Directory

--- goledger-challenge/components/Directory/Directory.component.jsx
+++ goledger-challenge/components/Directory/Directory.component.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import DirectoryContainer from './Directory.styles';
 import Products from '../templates/Products/Products.component';
 import Sellers from '../templates/Sellers/Sellers.component';
+import Categories from '../templates/Category/Categories.component';
 
 const Directory = () => {
   const router = useRouter();
@@ -11,7 +12,7 @@ const Directory = () => {
   return (
     <DirectoryContainer>
       {router.query.assetType === 'products' && <Products />}
-      {router.query.assetType === 'categories' && <div>category</div>}
+      {router.query.assetType === 'categories' && <Categories />}
       {router.query.assetType === 'sellers' && <Sellers />}
     </DirectoryContainer>
   );
